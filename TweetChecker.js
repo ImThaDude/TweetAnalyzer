5f5f05ab51a8d5acd0762d74106f74447b5d37a5
gpquanli <gpquanli@gmail.com>
2018-12-09 02:48:46 -0800

    Now you can add the links by strings on prompt.

--- TweetChecker.js
+++ TweetChecker.js
@@ -1,5 +1,6 @@
 var webdriver = require('selenium-webdriver');
 var firefox = require('selenium-webdriver/firefox');
+var prompt = require('prompt-async');
 var path = 'C:\\HDD Data\\Ewaco Analyzer\\Firefox Plugin\\geckodriver-v0.23.0-win64\\geckodriver.exe';
 
 var service = new firefox.ServiceBuilder(path);
@@ -10,29 +11,49 @@ builder.setFirefoxService(service);
 
 var driver = builder.build();
 
-async function test() {
+async function getTwitterData(twitterLink) {
     await driver.sleep(3000);
-    await driver.get("https://twitter.com/realDonaldTrump/status/1065088330969305089");
+    await driver.get(twitterLink);
     var tweeteruser = await driver.findElement(webdriver.By.className('username u-dir u-textTruncate')).getText();
     var retweets = await driver.findElement(webdriver.By.className('request-retweeted-popup')).getText();
     var favorited = await driver.findElement(webdriver.By.className('request-favorited-popup')).getText();
     var comments = await driver.findElement(webdriver.By.className('ProfileTweet-actionCount')).getAttribute('data-tweet-stat-count');
-    console.log(tweeteruser + ' ' + retweets + ' ' + favorited + ' ' + comments + ' Comments');
-    await driver.sleep(5000);
-    await driver.get('https://twitter.com/Ninja/status/958071040768344070');
-    tweeteruser = await driver.findElement(webdriver.By.className('username u-dir u-textTruncate')).getText();
-    retweets = await driver.findElement(webdriver.By.className('request-retweeted-popup')).getText();
-    favorited = await driver.findElement(webdriver.By.className('request-favorited-popup')).getText();
-    comments = await driver.findElement(webdriver.By.className('ProfileTweet-actionCount')).getAttribute('data-tweet-stat-count');
-    console.log(tweeteruser + ' ' + retweets + ' ' + favorited + ' ' + comments + ' Comments');
-    await driver.sleep(5000);
-    await driver.get('https://twitter.com/CouRageJD/status/1071517653351591936');
-    tweeteruser = await driver.findElement(webdriver.By.className('username u-dir u-textTruncate')).getText();
-    retweets = await driver.findElement(webdriver.By.className('request-retweeted-popup')).getText();
-    favorited = await driver.findElement(webdriver.By.className('request-favorited-popup')).getText();
-    comments = await driver.findElement(webdriver.By.className('ProfileTweet-actionCount')).getAttribute('data-tweet-stat-count');
-    console.log(tweeteruser + ' ' + retweets + ' ' + favorited + ' ' + comments + ' Comments');
-    //driver.close();
+    var day = new Date(Date.now()).toLocaleString();
+    console.log(day + ' ' + tweeteruser + ' ' + retweets + ' ' + favorited + ' ' + comments + ' Comments');
 }
 
-test();
+var time = 30;
+var tweetList = [];
+
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  }
+
+async function runInterator() {
+    for (i = 0; i < 20; i++) {
+        for (i = 0; i < tweetList.length; i++) {
+            //console.log(tweetList[i]);
+            try {
+                await getTwitterData(tweetList[i]);
+            } catch (err) {
+                console.log(err);
+                console.log('Continuing...');
+            }
+        }
+        console.log('\n');
+        await sleep(10000);
+    }
+}
+
+async function userInput() {
+    prompt.start();
+    for (i = 0; i < 10; i++) {
+        console.log("Insert twitter link...");
+        const {tweetlink} = await prompt.get("tweetlink");
+        console.log(tweetlink);
+        tweetList.push(tweetlink);
+    }
+}
+
+userInput();
+runInterator();
